2f936d0de6af7c1ca93a37b600a6ebc84d4f6157
Rutvik <rutvikchaudhary0110@gmail.com>
2023-02-04 22:31:57 +0530

    Implemented Company Bio CRUD APIs

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,7 @@
 const CANDIDATE = require('../models/Candidate.model')
 const COMPANY = require('../models/Company.model')
 const CANDIDATE_BIO = require('../models/Candidate_Bio.model')
+const COMPANY_BIO = require('../models/Company_Bio.model')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const otp_generator = require('otp-generator')
@@ -121,10 +122,9 @@ async function addCandidateBio(req, res) {
       _id: req.body.id,
       profile: req.body.profile,
       username: req.body.username,
-      bio: req.body.bio
+      bio: req.body.bio,
     })
-    return res.status(200).send({ status : "Bio Added Successfully"})
-    
+    return res.status(200).send({ status: 'Bio Added Successfully' })
   } catch (error) {
     return res.status(500).send({ error: 'Failed To Add Bio' })
   }
@@ -135,7 +135,7 @@ async function getCandidateBio(req, res) {
   try {
     let userBio = await CANDIDATE_BIO.findById(req.body.id)
     if (userBio) {
-      return res.status(200).send({ status : 'success', userBio})
+      return res.status(200).send({ status: 'success', userBio })
     }
     return res.status(500).send({ error: 'Failed To Fetch Candidate Bio' })
   } catch (error) {
@@ -145,19 +145,96 @@ async function getCandidateBio(req, res) {
 
 //  Update Candidate Bio route
 async function updateCandidateBio(req, res) {
-  let {profile, bio} = req.body
+  let { profile, bio } = req.body
   let updateBio = {}
-  if(profile){ updateBio.profile = profile}
-  if(bio){ updateBio.bio = bio}
+  if (profile) {
+    updateBio.profile = profile
+  }
+  if (bio) {
+    updateBio.bio = bio
+  }
   try {
     let userBio = await CANDIDATE_BIO.findById(req.body.id)
     if (userBio) {
-      let update = await CANDIDATE_BIO.findByIdAndUpdate(userBio.id, {$set : updateBio}, {new : true})
-      return res.status(200).send({ status : 'success', update})
+      let update = await CANDIDATE_BIO.findByIdAndUpdate(
+        userBio.id,
+        { $set: updateBio },
+        { new: true },
+      )
+      return res.status(200).send({ status: 'success', update })
     }
     return res.status(500).send({ error: 'Failed To Update Candidate Bio' })
   } catch (error) {
     return res.status(500).send({ error: 'Failed To Update Candidate Bio' })
+  }
+}
+
+//  Create Company Bio route
+async function addCompanyBio(req, res) {
+  try {
+    let companyBio = await COMPANY_BIO.findOne({ _id: req.body.id })
+    if (companyBio) {
+      return res.status(500).send({ error: 'Failed To Add Bio' })
+    }
+    companyBio = await COMPANY_BIO.create({
+      _id: req.body.id,
+      logo: req.body.logo,
+      website: req.body.website,
+      location: req.body.location,
+      description: req.body.description,
+      companySize: req.body.companySize,
+    })
+    return res.status(200).send({ status: 'Bio Added Successfully' })
+  } catch (error) {
+    return res.status(500).send({ error: 'Failed To Add Bio' })
+  }
+}
+
+//  Get Company Bio route
+async function getCompanyBio(req, res) {
+  try {
+    let companyBio = await COMPANY_BIO.findOne({ _id: req.body.id })
+    if (companyBio) {
+      return res.status(200).send({ status: 'sucess', companyBio })
+    }
+    return res.status(500).send({ error: 'Company Bio Not Found' })
+  } catch (error) {
+    return res.status(500).send({ error: 'Failed To Get Bio' })
+  }
+}
+
+//  Update Company Bio route
+async function updateCompanyBio(req, res) {
+  let { logo, website, location, description, companySize } = req.body
+  let updateBio = {}
+  if (logo) {
+    updateBio.logo = logo
+  }
+  if (website) {
+    updateBio.website = website
+  }
+  if (location) {
+    updateBio.location = location
+  }
+  if (description) {
+    updateBio.description = description
+  }
+  if (companySize) {
+    updateBio.companySize = companySize
+  }
+  try {
+    let companyBio = await COMPANY_BIO.findById(req.body.id)
+    if (companyBio) {
+      let update = await COMPANY_BIO.findByIdAndUpdate(
+        companyBio.id,
+        { $set: updateBio },
+        { new: true },
+      )
+      return res.status(200).send({ status: 'success', update })
+    }
+    return res.status(500).send({ error: 'Failed To Update Company Bio' })
+  } catch (error) {
+    return res.status(500).send({ error: 'Failed To Update Company Bio' })
   }
 }
 
@@ -169,5 +246,8 @@ module.exports = {
   addCandidateBio,
   getCandidateBio,
   updateCandidateBio,
+  addCompanyBio,
+  getCompanyBio,
+  updateCompanyBio,
   GenerateOtp,
 }
---
+++ server/models/Company_Bio.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const Company_Bio_Schema = new mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    unique: true,
+  },
+  logo: {
+    type: Buffer,
+  },
+  website: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  location: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+  },
+  companySize: {
+    type: Number,
+    require: true,
+  },
+})
+
+module.exports = mongoose.model('Company_Bio', Company_Bio_Schema)
